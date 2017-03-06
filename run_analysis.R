221b29407f50504d9ff82af52d84c713fae3e562
Alexey Yehorchenkov <alexee@ukr.net>
2017-03-06 13:05:55 +0200

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,6 @@
+## Connect library
+library(dplyr)
+
 ## file path
 # train data set
 file_train <- "./train/X_train.txt"
@@ -11,40 +14,45 @@ file_act_test <- "./test/y_test.txt"
 file_act_train <- "./train/y_train.txt"
 # activities names file
 file_act_names <- "activity_labels.txt"
+# subject fules
+file_subj_train <- "./train/subject_train.txt"
+file_subj_test <- "./test/subject_test.txt"
 
 ## Read files with appropriate column names
 col_names <- read.csv(file_names, header = FALSE, sep = "")
-train <- read.csv(file_train, header = FALSE, sep = "", col.names = col_names$V2)
-test <- read.csv(file_test, header = FALSE, sep = "", col.names = col_names$V2)
+train <- read.csv(file_train, header = FALSE, sep = "")
+test <- read.csv(file_test, header = FALSE, sep = "")
 act_names <- read.csv(file_act_names, header = FALSE, sep = "", col.names = c("N", "activity"))
 act_train <- read.csv(file_act_train, header = FALSE, sep = "")
 act_test <- read.csv(file_act_test, header = FALSE, sep = "")
+subj_train <- read.csv(file_subj_train, header = FALSE, sep = "", col.names = "subject")
+subj_test <- read.csv(file_subj_test, header = FALSE, sep = "", col.names = "subject")
 
-## Merge activities names with activity numbers
-m_act_train <- merge(act_train, act_names, by.x = "V1", by.y = "N")
-m_act_test <- merge(act_test, act_names, by.x = "V1", by.y = "N")
-# rename new column
-#colnames(m_act_train)[m_act_train$activity] <- "activity"
-#colnames(m_act_test)[m_act_test$activity] <- "activity"
+## Set names
+names(train) <- col_names[,2]
+names(test) <- col_names[,2]
 
 ## Merge data sets and activity
-train <- cbind(train, m_act_train)
-test <- cbind(test, m_act_test)
+train <- cbind(train, act_train)
+test <- cbind(test, act_test)
+train <- cbind(train, subj_train)
+test <- cbind(test, subj_test)
+
+## Merge activities names with activity numbers
+train <- merge(train, act_names, by.x = "V1", by.y = "N")
+test <- merge(test, act_names, by.x = "V1", by.y = "N")
 
 ## Merge test and train measurements
 mset <- rbind(train, test)
 
 ## Extract only the measurements on the mean and standard deviation for each measurement.
-# take neccessary column labels. I use escape "\\." to avoid such columns 'fBodyBodyGyroMag-meanFreq()'
-coln <- grep("mean\\.|std\\.|activity", names(mset), value = TRUE)
+# take neccessary column labels. I use escape "\\(" to avoid such columns 'fBodyBodyGyroMag-meanFreq()'
+coln <- grep("mean\\(|std\\(|activity|subject", names(mset), value = TRUE)
 # subset mset data test
 mset <- mset[, coln]
 
 ## create new data set with means for each activity and subject
-# split data set by activities
-s <- split(mset, mset$activity)
-# calculate mean for each column except the last "activity"
-new_mset <- sapply(s, function(x){colMeans(x[, 1:ncol(mset)-1], na.rm = TRUE)})
+new_mset <- mset %>% group_by(subject, activity) %>% summarise_each(funs(mean))
 
 ## The Result!
 new_mset
